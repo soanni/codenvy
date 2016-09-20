12c31e70eeb220e57a744eac300b8b14accd1422
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-09-20 17:50:08 +0300

    Add permissions filter for Synchronizer service (#790)

--- assembly/onpremises-ide-packaging-war-platform-api/src/main/java/com/codenvy/api/deploy/OnPremisesIdeApiServletModule.java
+++ assembly/onpremises-ide-packaging-war-platform-api/src/main/java/com/codenvy/api/deploy/OnPremisesIdeApiServletModule.java
@@ -95,7 +95,9 @@ public class OnPremisesIdeApiServletModule extends ServletModule {
                "/preferences",
                "/preferences/*",
                "/license",
-               "/license/*")
+               "/license/*",
+               "/ldap/sync",
+               "/ldap/sync/*")
                 .through(com.codenvy.auth.sso.client.LoginFilter.class);
 
         final Map<String, String> corsFilterParams = new HashMap<>();
