98e567703ad06f2ef32201d2ef0b075c4c0595cf
Valeriy Svydenko <vsvydenko@codenvy.com>
2016-09-21 18:25:01 +0300

    CHE-2369: add test

---
+++ plugins/plugin-hosted/codenvy-machine-hosted/src/test/java/com/codenvy/machine/WsAgentHealthCheckerWithAuthTest.java
@@ -0,0 +1,161 @@
+/*
+ *  [2012] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.machine;
+
+import com.codenvy.machine.authentication.shared.dto.MachineTokenDto;
+
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.model.machine.Machine;
+import org.eclipse.che.api.core.model.machine.MachineRuntimeInfo;
+import org.eclipse.che.api.core.model.machine.Server;
+import org.eclipse.che.api.core.rest.HttpJsonRequest;
+import org.eclipse.che.api.core.rest.HttpJsonRequestFactory;
+import org.eclipse.che.api.core.rest.HttpJsonResponse;
+import org.eclipse.che.api.workspace.shared.dto.AgentHealthStateDto;
+import org.eclipse.che.api.workspace.shared.dto.AgentStateDto;
+import org.mockito.Mock;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
+import static org.eclipse.che.api.machine.shared.Constants.WSAGENT_REFERENCE;
+import static org.eclipse.che.api.machine.shared.Constants.WS_AGENT_PORT;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author Valeriy Svydenko
+ */
+@Listeners(value = {MockitoTestNGListener.class})
+public class WsAgentHealthCheckerWithAuthTest {
+    private final static int    WS_AGENT_PING_CONNECTION_TIMEOUT_MS = 20;
+    private final static String WS_AGENT_SERVER_URL                 = "ws_agent";
+    private final static String API_ENDPOINT                        = "api_endpoint";
+
+    @Mock
+    private HttpJsonRequestFactory httpJsonRequestFactory;
+    @Mock
+    private Machine                devMachine;
+    @Mock
+    private MachineRuntimeInfo     machineRuntimeInfo;
+    @Mock
+    private Server                 server;
+    @Mock
+    private HttpJsonRequest        httpJsonRequest;
+    @Mock
+    private HttpJsonResponse       httpJsonResponse;
+
+    private Map<String, Server> servers = new HashMap<>(1);
+
+    private WsAgentHealthCheckerWithAuth checker;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        servers.put(WSAGENT_REFERENCE, server);
+        servers.put(WS_AGENT_PORT, server);
+
+        when(server.getRef()).thenReturn(WSAGENT_REFERENCE);
+        when(server.getUrl()).thenReturn(WS_AGENT_SERVER_URL);
+
+        checker = new WsAgentHealthCheckerWithAuth(httpJsonRequestFactory, WS_AGENT_PING_CONNECTION_TIMEOUT_MS, API_ENDPOINT);
+
+        when(httpJsonRequestFactory.fromUrl(anyString())).thenReturn(httpJsonRequest);
+        when(httpJsonRequest.setMethod(any())).thenReturn(httpJsonRequest);
+        when(httpJsonRequest.setTimeout(anyInt())).thenReturn(httpJsonRequest);
+        when(httpJsonRequest.request()).thenReturn(httpJsonResponse);
+
+        when(httpJsonResponse.getResponseCode()).thenReturn(200);
+        when(httpJsonResponse.asString()).thenReturn("response");
+
+        when(devMachine.getRuntime()).thenReturn(machineRuntimeInfo);
+        doReturn(servers).when(machineRuntimeInfo).getServers();
+    }
+
+    @Test
+    public void wsAgentIdShouldBeReturned() throws Exception {
+        assertEquals(WSAGENT_REFERENCE, checker.agentId());
+    }
+
+    @Test
+    public void stateShouldBeReturnedWithStatusNotFoundIfWorkspaceAgentIsNotExist() throws Exception {
+        when(machineRuntimeInfo.getServers()).thenReturn(emptyMap());
+
+        AgentHealthStateDto result = checker.check(devMachine);
+
+        assertEquals(WSAGENT_REFERENCE, result.getAgentId());
+        assertEquals(NOT_FOUND.getStatusCode(), result.getAgentState().getCode());
+    }
+
+    @Test(expectedExceptions = ServerException.class, expectedExceptionsMessageRegExp = "Workspace agent server not found in dev machine.")
+    public void shouldThrowExceptionOnCreatingPingRequestIfWsAgentIsNull() throws Exception {
+        servers.remove(WS_AGENT_PORT);
+
+        checker.check(devMachine);
+    }
+
+    @Test
+    public void pingRequestToWsAgentShouldBeSent() throws Exception {
+        MachineTokenDto machineTokenDto = mock(MachineTokenDto.class);
+        when(httpJsonResponse.asDto(MachineTokenDto.class)).thenReturn(machineTokenDto);
+        when(machineTokenDto.getMachineToken()).thenReturn("token");
+        when(httpJsonRequest.setAuthorizationHeader(eq("token"))).thenReturn(httpJsonRequest);
+
+        final AgentHealthStateDto result = checker.check(devMachine);
+        final AgentStateDto agentState = result.getAgentState();
+
+        verify(httpJsonRequestFactory).fromUrl(WS_AGENT_SERVER_URL + '/');
+        verify(httpJsonRequest, times(2)).setMethod(javax.ws.rs.HttpMethod.GET);
+        verify(httpJsonRequest).setTimeout(WS_AGENT_PING_CONNECTION_TIMEOUT_MS);
+        verify(httpJsonRequest, times(2)).request();
+
+        assertEquals(WSAGENT_REFERENCE, result.getAgentId());
+        assertEquals(200, agentState.getCode());
+        assertEquals("response", agentState.getReason());
+    }
+
+    @Test
+    public void returnResultWithUnavailableStateIfDoNotGetResponseFromWsAgent() throws Exception {
+        doThrow(IOException.class).when(httpJsonRequest).request();
+
+        final AgentHealthStateDto result = checker.check(devMachine);
+        final AgentStateDto agentState = result.getAgentState();
+
+        verify(httpJsonRequestFactory).fromUrl(WS_AGENT_SERVER_URL + '/');
+        verify(httpJsonRequest, times(2)).setMethod(javax.ws.rs.HttpMethod.GET);
+        verify(httpJsonRequest).setTimeout(WS_AGENT_PING_CONNECTION_TIMEOUT_MS);
+        verify(httpJsonRequest, times(2)).request();
+
+        assertEquals(WSAGENT_REFERENCE, result.getAgentId());
+        assertEquals(SERVICE_UNAVAILABLE.getStatusCode(), agentState.getCode());
+    }
+}
+
